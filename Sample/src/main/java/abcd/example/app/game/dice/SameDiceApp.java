a554dbd9f5dd314e267ee80e3695b6d635eaf2e3
Kadir Ensari <125377706+kadirensari@users.noreply.github.com>
2023-12-31 17:32:31 +0100

    Restructure

---
+++ Sample/src/main/java/abcd/example/app/game/dice/SameDiceApp.java
@@ -0,0 +1,15 @@
+package abcd.example.app.game.dice;
+
+import abcd.example.game.dice.SameDice;
+
+public class SameDiceApp {
+    public static void main(String[] args)
+    {
+        run();
+    }
+
+    public static void run()
+    {
+
+    }
+}
---
+++ Sample/src/main/java/abcd/example/game/dice/SameDice.java
@@ -0,0 +1,11 @@
+package abcd.example.game.dice;
+
+public class SameDice {
+    private double probability;
+
+    public void play()
+    {
+
+    }
+
+}
