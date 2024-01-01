547a8c95109e8fab0ecfad7ef5f9900968502ed0
Kadir Ensari <125377706+kadirensari@users.noreply.github.com>
2024-01-01 01:27:41 +0100

    Same dice simulation added

--- Sample/src/main/java/abcd/example/app/game/dice/SameDiceApp.java
+++ Sample/src/main/java/abcd/example/app/game/dice/SameDiceApp.java
@@ -1,6 +1,8 @@
 package abcd.example.app.game.dice;
 
-import abcd.example.game.dice.SameDice;
+import abcd.example.simulation.dice.SameDiceSimulation;
+import java.util.Random;
+import java.util.Scanner;
 
 public class SameDiceApp {
     public static void main(String[] args)
@@ -8,8 +10,24 @@ public class SameDiceApp {
         run();
     }
 
-    public static void run()
+    static void run()
     {
+        Scanner kb = new Scanner(System.in);
+        Random r = new Random();
 
+        while(true) {
+            System.out.print("How many times to play?: ");
+            int n = kb.nextInt();
+
+            if (n == 0)
+                break;
+
+            SameDiceSimulation s = new SameDiceSimulation();
+            s.play(n, r);
+        }
+
+        System.out.println("-----------------------------------");
+        System.out.println("The End");
+        System.out.println("-----------------------------------");
     }
 }
---
+++ Sample/src/main/java/abcd/example/simulation/dice/SameDiceSimulation.java
@@ -0,0 +1,23 @@
+package abcd.example.simulation.dice;
+
+import java.util.Random;
+
+public class SameDiceSimulation {
+
+    public void play(int n, Random r)
+    {
+        int win = 0;
+        for(int i = 0; i < n; i++)
+            if(r.nextInt(1,7) == r.nextInt(1,7))
+                win++;
+        printResults(win, n);
+    }
+
+    private void printResults(int win, int n)
+    {
+        System.out.println("-----------------------------------");
+        System.out.printf("Win prob --> %f%n", (double)win/n);
+        System.out.println("-----------------------------------");
+    }
+
+}
