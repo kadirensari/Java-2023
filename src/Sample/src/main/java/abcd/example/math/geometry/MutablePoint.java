d2c74a3a4098ed73c91b04cf008f613ebc22b364
Kadir Ensari <125377706+kadirensari@users.noreply.github.com>
2024-02-22 22:06:25 +0100

    Update
    MutablePoint & Point

--- src/Sample/src/main/java/abcd/example/math/geometry/MutablePoint.java
+++ src/Sample/src/main/java/abcd/example/math/geometry/MutablePoint.java
@@ -1,4 +1,91 @@
 package abcd.example.math.geometry;
 
 public final class MutablePoint {
+    private double x;
+    private double y;
+
+    MutablePoint(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    private MutablePoint(double a, double b, boolean polar)
+    {
+        if(polar) {
+            x = a * Math.cos(b);
+            y = a * Math.sin(b);
+        }
+        else {
+            x = a;
+            y = b;
+        }
+    }
+
+    public static MutablePoint createCartesian(double x, double y)
+    {
+        return new MutablePoint(x, y, false);
+    }
+
+    public static MutablePoint createPolar(double radius, double theta)
+    {
+        return new MutablePoint(radius, theta, true);
+    }
+
+    public double getX()
+    {
+        return x;
+    }
+
+    public void setX(double x)
+    {
+        this.x = x;
+    }
+
+    public double getY()
+    {
+        return y;
+    }
+
+    public void setY(double y)
+    {
+        this.y = y;
+    }
+
+    public double distance()
+    {
+        return distance(0, 0);
+    }
+
+    public double distance(MutablePoint other)
+    {
+        return distance(other.x, other.y);
+    }
+
+    public double distance(double x, double y)
+    {
+        return PointCommon.distance(this.x, this.y, x, y);
+    }
+
+    public void offset(double dx, double dy)
+    {
+        this.x += dx;
+        this.y += dy;
+    }
+
+    public Point toPoint()
+    {
+        return new Point(this.x, this.y);
+    }
+
+    public boolean equals(Object other)
+    {
+        return other instanceof MutablePoint p && PointCommon.equals(this.x, this.y, p.x, p.y);
+    }
+
+    public String toString()
+    {
+        return PointCommon.toString(this.x, this.y);
+    }
+
 }
--- src/Sample/src/main/java/abcd/example/math/geometry/Point.java
+++ src/Sample/src/main/java/abcd/example/math/geometry/Point.java
@@ -1,4 +1,75 @@
 package abcd.example.math.geometry;
 
 public final class Point {
+    private final double x;
+    private final double y;
+
+    Point(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    private Point(double a, double b, boolean polar)
+    {
+        if(polar) {
+            x = a * Math.cos(b);
+            y = a * Math.sin(b);
+        }
+        else {
+            x = a;
+            y = b;
+        }
+    }
+
+    public static Point createCartesian(double x, double y)
+    {
+        return new Point(x, y, false);
+    }
+
+    public static Point createPolar(double radius, double theta)
+    {
+        return new Point(radius, theta, true);
+    }
+
+    public double getX()
+    {
+        return x;
+    }
+
+    public double getY()
+    {
+        return y;
+    }
+
+    public double distance(double x, double y)
+    {
+        return PointCommon.distance(this.x, this.y, x, y);
+    }
+
+    public double distance()
+    {
+        return distance(0, 0);
+    }
+
+    public double distance(Point other)
+    {
+        return distance(other.x, other.y);
+    }
+
+    public MutablePoint toMutablePoint()
+    {
+        return new MutablePoint(this.x, this.y);
+    }
+
+    public boolean equals(Object other)
+    {
+        return other instanceof Point p && PointCommon.equals(this.x, this.y, p.x, p.y);
+    }
+
+    public String toString()
+    {
+        return PointCommon.toString(this.x, this.y);
+    }
+
 }
